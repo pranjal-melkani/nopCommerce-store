d9c216edb9b8bf655b46d42b2e7578e1b175e9db
Pranjal <pranjalmelkani18@gmail.com>
2025-03-14 12:12:48 +0530

    basedriver and conftest added

---
+++ testcases/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from configparser import ConfigParser
+
+
+@pytest.fixture(scope='session', autouse=True)
+def driver():
+    config = ConfigParser()
+    config.read('../pytest.ini')
+    URL = config.get('browser', 'URL')
+    browser = webdriver.Chrome()
+    browser.maximize_window()
+    browser.get(URL)
+    yield browser
+    browser.quit()
